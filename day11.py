f3c67e420580726739d191dfafcafc5be57de98a
Alasdair Nicol <alasdair@thenicols.net>
2024-12-15 12:32:38 +0000

    Day 11 parts 1 and 2

---
+++ day11.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from collections import Counter, defaultdict
+
+
+def next_number(x: int) -> tuple[int, ...]:
+    if x == 0:
+        return (1,)
+    if (xlen := len(str(x))) % 2 == 0:
+        return int(str(x)[: xlen // 2]), int(str(x)[xlen // 2 :])
+    else:
+        return (x * 2024,)
+
+
+def do_blinks(counts: dict, blinks: int) -> dict:
+    for _ in range(blinks):
+        new_counts: dict = defaultdict(int)
+        for k, count in counts.items():
+            for new_k in next_number(k):
+                new_counts[new_k] += count
+        counts = new_counts
+    return counts
+
+
+def main():
+    input = read_input()
+    numbers = [int(x) for x in input.split()]
+    counts = Counter(numbers)
+
+    part_1 = sum(do_blinks(counts, 25).values())
+    print(f"{part_1=}")
+
+    part_2 = sum(do_blinks(counts, 75).values())
+    print(f"{part_2=}")
+
+
+def read_input() -> str:
+    with open("day11.txt") as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    main()
